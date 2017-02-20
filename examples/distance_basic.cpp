721cfa951dfb993af7a55cfa636ae681fabb0100
Michał Zieliński <michal@zielinscy.org.pl>
2017-02-20 16:29:19 +0100

    fix examples

--- examples/distance_basic.cpp
+++ examples/distance_basic.cpp
@@ -1,7 +1,7 @@
-@MODULE: DistanceSensor
+@CATEGORY: DistanceSensor
 @NAME: basic
 @PORTS: stm32
-@BOARDS: robocore,core2,core2mini
+@BOARDS: core2,core2mini
 #include <hFramework.h>
 #include <DistanceSensor.h>
 
@@ -9,11 +9,29 @@ using namespace hModules;
 
 void hMain()
 {
-{% if board(robocore) %}
-	DistanceSensor sens(hSens1.getBaseSens());
-{% else %}
 	DistanceSensor sens(hSens1);
-{% endif %}
+
+	for (;;)
+	{
+		int dist = sens.getDistance();
+		Serial.printf("%d\r\n", dist);
+		sys.delay(200);
+	}
+}
+
+#####
+@CATEGORY: DistanceSensor
+@NAME: basic
+@PORTS: stm32
+@BOARDS: robocore
+#include <hFramework.h>
+#include <DistanceSensor.h>
+
+using namespace hModules;
+
+void hMain()
+{
+	DistanceSensor sens(hSens1.getBaseSens());
 
 	for (;;)
 	{
