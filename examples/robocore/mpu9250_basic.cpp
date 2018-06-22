0647c37dd69d15f46bd3e68a4518975977f70acf
Hubert424 <hubert4242@gmail.com>
2018-06-22 11:41:31 +0200

    examples update

--- examples/robocore/mpu9250_basic.cpp
+++ examples/robocore/mpu9250_basic.cpp
@@ -7,41 +7,6 @@ void hMain()
 {
 	sys.setLogDev(&Serial);
 
-	MPU9250 mpu(hSens1);
-	mpu.enableInterrupt();
-	mpu.init();
-
-	for (;;)
-	{
-		mpu.waitForData();
-
-		if (mpu.process())
-		{
-			int16_t ax, ay, az, gx, gy, gz, mx, my, mz;
-			mpu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-
-			printf("\n");
-			printf("ax %6d ay %6d az %6d \r\n", ax, ay, az);
-			printf("gx %6d gy %6d gz %6d \r\n", gx, gy, gz);
-			printf("mx %6d my %6d mz %6d \r\n", mx, my, mz);
-		}
-	}
-}
-
-#####
-@CATEGORY: MPU9250
-@NAME: basic
-@PORTS: stm32
-@BOARDS: robocore
-#include <hFramework.h>
-#include <MPU9250.h>
-
-using namespace hModules;
-
-void hMain()
-{
-	sys.setLogDev(&Serial);
-
 	MPU9250 mpu(hSens1.getBaseSens());
 	mpu.enableInterrupt();
 	mpu.init();
