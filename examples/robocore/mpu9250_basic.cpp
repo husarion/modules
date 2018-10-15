44d9527228bdf7f521c7ef70e469fc79350a10b2
Hubert Zwiercan <hubert4242@gmail.com>
2018-10-15 10:19:27 +0200

    MPU example update

--- examples/robocore/mpu9250_basic.cpp
+++ examples/robocore/mpu9250_basic.cpp
@@ -5,25 +5,22 @@ using namespace hModules;
 
 void hMain()
 {
-	sys.setLogDev(&Serial);
+    sys.setLogDev(&Serial);
 
-	MPU9250 mpu(hSens1.getBaseSens());
-	mpu.enableInterrupt();
-	mpu.init();
+    MPU9250 mpu(hSens1.getBaseSens());
+    mpu.init();
 
-	for (;;)
-	{
-		mpu.waitForData();
-
-		if (mpu.process())
-		{
-			int16_t ax, ay, az, gx, gy, gz, mx, my, mz;
-			mpu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
-
-			printf("\n");
-			printf("ax %6d ay %6d az %6d \r\n", ax, ay, az);
-			printf("gx %6d gy %6d gz %6d \r\n", gx, gy, gz);
-			printf("mx %6d my %6d mz %6d \r\n", mx, my, mz);
-		}
-	}
+    for (;;)
+    {
+        if (mpu.process())
+        {
+            int16_t ax, ay, az, gx, gy, gz, mx, my, mz;
+            mpu.getMotion9(&ax, &ay, &az, &gx, &gy, &gz, &mx, &my, &mz);
+            sys.delay(500);
+            printf("\n");
+            printf("ax %6d ay %6d az %6d \r\n", ax, ay, az);
+            printf("gx %6d gy %6d gz %6d \r\n", gx, gy, gz);
+            printf("mx %6d my %6d mz %6d \r\n", mx, my, mz);
+        }
+    }
 }
